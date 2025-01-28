dd3c098308569f5347d79f87c575ee2b54e559ec
Aashish Anantharaman <68557624+ABlizzard07@users.noreply.github.com>
2025-01-28 00:59:36 -0500

    commit test

--- app/index.tsx
+++ app/index.tsx
@@ -6,7 +6,7 @@ import { Link } from 'expo-router';
 const Welcome = () => {
   return (
     <View className="flex-1 justify-center items-center bg-tan">
-      <Text className="font-bold text-3xl mb-10 text-red-500">Welcome to XyloWords!</Text>
+      <Text className="font-bold text-3xl mb-10 text-red-500">Welcome to XyloWords, player!</Text>
       <Link href="./(tabs)/play" asChild>
         <Button title="Sign In" />
       </Link>
