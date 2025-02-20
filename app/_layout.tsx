cf52d6c12ba9212c9fac0703f0e0701c5a254259
Aashish Anantharaman <68557624+ABlizzard07@users.noreply.github.com>
2025-02-19 22:33:39 -0500

    package updates

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,7 @@ import { Stack } from 'expo-router';
 import "../global.css";
 
 export default function RootLayout() {
+
   return (
     <Stack>
       <Stack.Screen 
--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,20 @@
-// app/index.tsx
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import { Link } from 'expo-router';
 
 const Welcome = () => {
   return (
-    <View className="flex-1 justify-center items-center bg-tan">
-      <Text className="font-bold text-3xl mb-10 text-red-500">Welcome to XyloWords, player!</Text>
+    <View className="flex-1 justify-center items-center bg-tan text-center">
+      <Image 
+        source={require('../assets/logo.png')} 
+        className="w-60 h-60 mb-10" 
+      />
+      <Text className="font-bold text-3xl mb-5">Xylowords</Text>
+      <Text className="text-xl mb-10 w-2/3 text-center">Reach new English heights in this epic learning arena.</Text>
       <Link href="./(tabs)/play" asChild>
-        <Button title="Sign In" />
+        <TouchableOpacity className="bg-[#ffd700] border-2 border-[#a45a45] rounded-full py-2 px-4">
+          <Text className="text-white text-lg font-bold">Get Started</Text>
+        </TouchableOpacity>
       </Link>
     </View>
   );
